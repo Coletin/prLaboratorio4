cb7a74fe2a0e3887e1241a715a3bf58c9d9360ea
GRode <40580626+Coletin@users.noreply.github.com>
2022-06-07 21:09:47 -0300

    subida cambios

---
+++ Clases/Hostal.cpp
@@ -0,0 +1,115 @@
+#include "../Clases/Hostal.h"
+#include "../Clases/Habitacion.h"
+#include "../Clases/Calificacion.h"
+#include "../Clases/Estadia.h"
+
+string Hostal::getNombre(){ return nombre; }
+void Hostal::setNombre(string _nombre){ nombre = _nombre; }
+string Hostal::getDireccion(){ return direccion; }
+void Hostal::setDireccion(string _direccion){ direccion = _direccion; }
+string Hostal::getTelefono(){ return telefono; }
+void Hostal::setTelefono(string _telefono){ telefono = _telefono; }
+
+Hostal::Hostal(){};
+Hostal::Hostal(string _nombre, string _direccion, string _telefono){
+    nombre = _nombre;
+    direccion = _direccion;
+    telefono = _telefono;    
+    set<Habitacion*> listaHabitaciones;
+    habitaciones = listaHabitaciones;
+    set<Calificacion*> listaCalificaciones;
+    calificaciones = listaCalificaciones;
+}
+
+set<DTEstadia*> Hostal::getEstadiasDT(){
+    set<DTEstadia*> respuesta;
+    return respuesta;
+}
+
+DTHabitacion Hostal::getHabDis(DataR datar){
+    DTHabitacion respuesta = DTHabitacion();
+    return respuesta;
+}
+
+Habitacion Hostal::getHabNum(int numeroHab){
+    Habitacion respuesta = Habitacion();
+    return respuesta;
+}
+
+float Hostal::getPromCal(){
+    return 0.0;
+}
+
+set<DTCalificacion*> Hostal::getCalifs(){
+    set<DTCalificacion*> respuesta;
+    return respuesta;
+}
+
+set<DTHabitacion*> Hostal::getHabitaciones(){
+    set<DTHabitacion*> respuesta;
+    return respuesta;
+}
+
+DTHostal Hostal::getDT(){
+    set<DTCalificacion*> califica = getCalifs();
+    float prom = getPromCal();
+    DTHostal respuesta = DTHostal(nombre,direccion,telefono,prom,califica);
+    return respuesta;
+}
+
+bool Hostal::existeEstadiasActivas(string _email, string _nombre){
+    return false;
+}
+
+set<DTReserva*> Hostal::getReservasFinalizadasAsociadas(string _email, string _nombre){
+    set<DTReserva*> respuesta;
+    return respuesta;
+}
+
+set<DTReserva*> Hostal::getReservasAsociadas(string _nombre, string _email){
+    set<DTReserva*> respuesta;
+    return respuesta;
+}
+
+void Hostal::agregarCalificacion(Calificacion cal){
+    //calificaciones.insert(cal);
+}
+
+int Hostal::getHabEstadia(Estadia est){
+    return 1;
+}
+
+
+
+
+/*
+class Hostal{
+    private:
+        string nombre;
+        string direccion;
+        string telefono;
+        set<Habitacion*> habitaciones;
+        set<Calificacion*> calificaciones;
+    public:
+        Hostal();
+        Hostal(string _nombre, string _direccion, string _telefono);
+        void setNombre(string _nombre);
+        void setDireccion(string _direccion);
+        void setTelefono(string _telefono);
+        string getNombre();
+        string getDireccion();
+        string getTelefono();
+        set<DTEstadia*> getEstadiasDT();
+        DTHabitacion getHabDis(DataR datar);
+        Habitacion getHabNum(int numeroHab);
+        float getPromCal();
+        set<DTCalificacion*> getCalifs();
+        set<DTHabitacion*> getHabitaciones();
+        DTHostal getDT();
+        bool existeEstadiasActivas(string email, string nombre);
+        set<DTReserva*> getReservasFinalizadasAsociadas(string email, string nombre);
+        set<DTReserva*> getReservasAsociadas(string nombre, string email);
+        void agregarCalificacion(Calificacion cal);
+        int getHabEstadia(Estadia est);
+};
+*/
--- Clases/Reserva.h
+++ Clases/Reserva.h
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 #include <set>
-#include "tipos.h"
+#include "../Tipos/tipos.h"
+#include "../Clases/Estadia.h"
 
 class Reserva{
     private:
@@ -13,14 +14,15 @@ class Reserva{
      set<Huesped*> huespedes;
     public:
      Reserva(int,DTFecha,DTFecha,EstadoReserva); 
+     Reserva();
      ~Reserva();
      virtual float calcularCosto() = 0;
      int getCodigo();
      DTFecha getCheckIn();
      DTFecha getCheckOut();
      EstadoReserva getEstado();
-     void setCodigo(int&);
-     void setCheckIn(DTFecha&);
-     void setCheckOut(DTFecha&);
-     void setEstado(EstadoReserva&);
+     void setCodigo(int);
+     void setCheckIn(DTFecha);
+     void setCheckOut(DTFecha);
+     void setEstado(EstadoReserva);
 };
